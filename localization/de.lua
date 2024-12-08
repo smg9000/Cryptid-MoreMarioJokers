613128871e6efc53ae60f8f3f94eef61ca8c0194
Foegro <68813500+Foegro@users.noreply.github.com>
2024-12-08 18:51:14 +0100

    Adds German Localisation

---
+++ localization/de.lua
@@ -0,0 +1,35 @@
+return {
+    descriptions = {
+        Joker = {
+            j_mmj_nluigi = {
+                name = "Luigi",
+                text = {
+                    "Alle Joker geben",
+                    "{X:chips,C:white} X#1# {} Chips",
+					"{C:inactive}Wird noch überarbeitet!!{} ",
+                },
+            },
+			j_mmj_mario = {
+                name = "Mario",
+                text = {
+                    "Löse alle Joker",
+                    "{C:attention}#1#{} weitere(s) mal aus",
+                },
+            },
+			j_mmj_waluigi = {
+                name = "Waluigi",
+                text = {
+                    "Alle Joker geben",
+                    "{X:mult,C:white} X#1# {} Mult",
+                },
+            },
+            j_mmj_wario = {
+                name = "Wario",
+                text = {
+                    "Alle Joker geben",
+                    "{C:money}$#1#{} wenn ausgelöst",
+                },
+            },
+        }    
+    }
+}
